00fdc61579518458d8d9d99e80bdc45497a8beb0
Aleia <aleiaknight@gmail.com>
2020-03-23 14:21:32 -0400

    displaying all data"

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import data_analysis
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 
@@ -11,10 +13,9 @@ app = Flask(__name__)
 def home():
     '''Main page setup function'''
     # print(data_analysis.meteor)
-    meteors = data_analysis.meteor_kv
+    meteors = data_analysis.meteor_kv  # take data from data_analysis.py
+    # print(meteors)    
     return render_template('index.html', meteors = meteors)
-
-
 
 if __name__=='__main__':
     app.run(Debug = True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
