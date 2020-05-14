d1b24fdc301af3e39f02a6f956390a27b14deee7
Aleia Knight <aleiaknight@Sarpedon.local>
2020-05-14 10:14:40 -0700

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import matplotlib.pyplot as plt
 import pandas as pd
-import data_analysis
+# import data_analysis
 import os
 
 
@@ -15,22 +15,22 @@ app = Flask(__name__)
 def home():
     '''Main page setup function'''
     # print(data_analysis.meteor)
-    meteor_kv = data_analysis.meteor_kv  # take data from data_analysis.py
+    # meteor_kv = data_analysis.meteor_kv  # take data from data_analysis.py
     # meteor = data_analysis.meteor # take data from data_analysis.py
         # print(meteors)    
-    return render_template('index_new.html', meteor_kv = meteor_kv)
+    return render_template('index_new.html') # meteor_kv = meteor_kv
 
 
 
 
-@app.route('/table')
-def table():
-    meteor_2000 = data_analysis.api_meteor_data
-    html = meteor_2000.to_html()
-    text_file = open("templates/table.html", "w")
-    text_file.write(html)
-    text_file.close()
-    return render_template('table.html', html = html)
+# @app.route('/table')
+# def table():
+#     # meteor_2000 = data_analysis.api_meteor_data
+#     # html = meteor_2000.to_html()
+#     text_file = open("templates/table.html", "w")
+#     text_file.write(html)
+#     text_file.close()
+#     return render_template('table.html', html = html)
 
 
 
@@ -39,9 +39,11 @@ def table():
 def visuals():
     return render_template('visuals.html')
 
-@app.route('/notebook')
-def notebook():
-    return render_template('data_analysis_web.html')
+
+
+# @app.route('/notebook')
+# def notebook():
+#     return render_template('data_analysis_web.html')
 
 
 
--- data_analysis.py
+++ data_analysis.py
@@ -1,245 +1,329 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Analyzing Meteorite Dataset 
+# # ***Premise***
+# ---
+# ---
+
+# In[ ]:
+
+
+
+
+
+# # ***Questions***
+# ---
+# ---
+
+# + **Easy**
+#     + Which year had the most meteors?
+#         - _2013_
+#     + What is the average mass of a meteors we've observed?
+# + **Hard**
+#     + Are there more meteors around the equator or the poles?
+#     + What time of year got the most asteroids? (Noticable trends in year data)
+# + **Other**
+#     + How many meteors fell in important years (birth year(1995), Y2K, 2008(Obama), 2020(Now))?
+# 
+
+# # ***Concepts to cover***
+# ---
+# ---
+
+# - [x] Histograms
+# - [ ] PDF
+# - [ ] CDF
+# - [ ] Hypothesis Testing
+# - [ ] Confidence Interval
+# - [ ] Correlation
+# - [ ] Outliers
+# - [ ] Normal Distribution
+# - [ ] Time Analysis
+
+# # ***Imports***
+# ---
+# ---
+
+# In[11]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from scipy import stats
+import plotly.express as px
+from geopy.geocoders import Nominatim
+import geopy as gp
+from datetime import datetime
+
+
+# # ***Data***
+# ---
+# ---
+
+# In[17]:
+
+
+data = pd.read_csv('data/meteorite-landings.csv')
+# data = pd.read_csv('../data/Meteorite_Landings.csv')
+print(data.columns)
+
+data.head()
+
+
+# ## Basic Statistics
+
+# In[18]:
+
+
+print("Data described: \n")
+print(data.describe())
+print('\n')
+print("Data info: \n")
+print(data.info())
+
+
+# *Conclusions / Questions*
+#     - There are missing values in mass, year, and locations
+#     - Appropriate data types thus far
+
+# # ***Cleaning***
+# ---
+# ---
+# 
+
+# ## Rename columns
+
+# In[19]:
+
+
+data.rename(columns={'recclass':'class', 'reclat':'lat', 'reclong':'long', 'mass (g)':'mass'}, inplace=True)
+data
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## Outliers
+
+# In[20]:
+
+
+sns.boxplot(x=data['year'])
+
+
+# In[21]:
+
+
+fig, ax = plt.subplots(figsize=(16,8))
+ax.scatter(data['year'], data['mass'])
+# ax.set_xlabel('Proportion of non-retail business acres per town')
+# ax.set_ylabel('Full-value property-tax rate per $10,000')
+plt.show()
+
+
+# # ***Statistical Plotting***
+# ---
+# ---
+
+# ## Fall vs Fallen Histogram
+
+# In[22]:
+
+
+data['fall'].hist(bins=3)  # 
+plt.show()
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## Equator or Poles
+
+# In[23]:
+
+
+plt.scatter(data.mass, data.lat)
+# print(data[data['lat']>0].count())
+# print(data[data['lat']<0].count())
+axes = plt.gca()
+axes.set_ylim([-90,90])
+above_equator = data[data.lat >0].shape[0]
+at_equator = data[data.lat ==0].shape[0]
+below_equator = data[data.lat <0].shape[0]
+plt.show()
+
+print("Above Equator:", above_equator, '\n')
+print("At Equator:", at_equator, '\n')
+print("Below Equator:", below_equator, '\n')
+
+labels = ["Above", 'At', 'Below']
+values = [above_equator, at_equator, below_equator]
+plt.pie(values, labels=labels)
+plt.show()
+
+
+# *Conclusions / Questions*
+#     - There are missig values because of the `Nan` values that I didn't remove. If I do remove then and replace them with 0, then it'ss alter results of values that actually are on the equator or prime meridian
+
+# ## Box Plot
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## PDF
+
+# In[10]:
+
+
+sns.distplot(data['year'].dropna(), hist=True, kde=True, bins=16)
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## CDF
+
+# In[24]:
+
+
+ls_year = data['year'].dropna().values
+
+def calculate_cdf(x, threshold):
+    return np.sum(x <= threshold)
+
+# Create an array cdf_age where each value is the cdf of the age for each threshold
+cdf_year = [calculate_cdf(ls_year, r)/len(ls_year) for r in range(int(np.min(ls_year)), int(np.max(ls_year)))]
+
+plt.plot(range(int(np.min(ls_year)), int(np.max(ls_year))), cdf_year)
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## Violin Plot
+
+# In[12]:
+
+
+sns.violinplot(x="year", y="mass", data=data)
+
+
+# *Conclusions / Questions*
+#     - Why are the masses in the violin plot negative?
+
+# ## Correlation
+
+# In[13]:
+
+
+sns.heatmap(data.corr(), annot=True, cmap='coolwarm')
+
+
+# *Conclusions / Questions*
+#     - Why are the masses in the violin plot negative?
+
+# # ***Various Plotting***
+# ---
+# ---
+
+# ## See the top 10 classification of meteors
+
+# In[14]:
+
+
+top_10_class = data['class'].value_counts()[:10]
+plt.bar(top_10_class, height = 1)
+
+top_10_class.plot(kind='bar')
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## Lat and Long scatter plot, using mass as bubble size
+# - ***Latitude are y values  (90 through -90)***
+# - ***Longitude are x values (-180 to 180)***
+
+# In[25]:
+
+
+plt.figure(figsize=(10,7))
+N = len(data['mass'])
+area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+colors = np.random.rand(N)
+plt.scatter(data['long'], data['lat'], s=area, c=colors, alpha=0.2)
+
+plt.grid(True)
+axes = plt.gca()
+axes.set_xlim([-90,90])
+axes.set_ylim([-180,180])
+plt.show()
+
+
+# *Conclusions / Questions*
+#     - ...
+
+# ## Geolocation Function using geopy
+# 
+
+# In[ ]:
+
+
+# geolocator = Nominatim(user_agent="project_impact")
+# coor=gp.Point(data['lat'][1], data['long'][1])
+# location = geolocator.reverse(coor)
+# print(location.raw['address'].get('country'))
+
+
+# ## Geolocations loop
+
+# In[ ]:
+
+
+# lists = []
+# for i in range(20):
+#     lats = data['lat'].get(key = i)
+#     longs = data['long'].get(key = i)
+#     coor = gp.Point(lats, longs)
+#     country = geolocator.reverse(gp.Point(coor)).raw['address'].get('country')
+#     lists.append(country)
+# print(lists)
+
+
+# ## Year Value Counts
 
 # In[16]:
 
 
-# imports
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import timeit   # for testing code runtime
-from statistics import mode 
-# from geopy.geocoders import Nominatim  # for geolocation conversion
-import csv
-import sys
-sys.executable
+print(data['year'].value_counts())
+# data['recclass'].value_counts().plot(kind='bar')  # bar chart of the amount embarked passengers by class
 
 
-# # Important Variables
+# ## Time Analysis
 
-# * meteor 
-# * api_meteor_data
-# * api_meteor_data_arr
-# * meteor_test
-# * meteor_test_2000 - 
+# In[17]:
 
-# In[5]:
 
+data['year'].fillna(0).astype(int)
 
-meteor = pd.read_csv('data/meteorite-landings.csv')
-meteor = pd.DataFrame(meteor)
 
+# *Conclusions / Questions*
+#     - ...
 
-# print(meteor)
-# print("\n\n\n")
+# In[18]:
 
-# # print(meteor.describe())
-# meteor.info()
-# print("\n\n\n")
 
-# #list all column names
-# print(list(meteor.columns))
+year_count = data.groupby('year')['year'].count()
+plt.figure(figsize=(10,8))
+plt.plot(year_count)
+print("Years describes: ", year_count.describe(), '\n')
+print("Confirming amount of unique year", data.year.nunique(), '\n')
+print("Year with most recorded data:", data.year.max())
+print("Total span of year data: ", data.year.max()-data.year.min(), 'years\n')
+# plt.hist(year_count)
 
-# print("\n\n\n")
-# # print(meteor['fall'])
 
-# print(np.max(meteor['year']))
-# print(np.min(meteor['year']))
-# print(mode(meteor['year']))
-
-
-# # visualizing mass
-
-# In[6]:
-
-
-
-# print("Maximum mass: ", np.max(meteor['mass']))
-# print("Minimum mass: ", np.min(meteor['mass']))
-# print("Mean mass: ",np.mean(meteor['mass']))
-
-
-# # create a figure and axis 
-# fig, ax = plt.subplots() 
-
-
-# # scatter the sepal_length against the sepal_width
-# ax.scatter(meteor['mass'], meteor['year'])
-# # set a title and labels
-# ax.set_title('Meteor Dataset (mass vs year)')
-# ax.set_xlabel('mass')
-# ax.set_ylabel('year')
-
-
-# # # Time Analysis Testing
-
-# # In[6]:
-
-
-# # Testing a time analysis function
-# import timeit
-# code_to_test = """
-# a = range(100000)
-# b = []
-# for i in a:
-#     b.append(i*2)
-# """
-# elapsed_time = timeit.timeit(code_to_test, number=100)/100
-# print(elapsed_time)
-
-
-# # In[8]:
-
-
-
-# bar_chart_time="""
-# # create a figure and axis 
-# fig, ax = plt.subplots() 
-
-
-# # scatter the sepal_length against the sepal_width
-# ax.scatter(meteor['mass'], meteor['year'])
-# # set a title and labels
-# ax.set_title('Meteor Dataset (mass vs year)')
-# ax.set_xlabel('mass')
-# ax.set_ylabel('year')
-# """
-
-
-# 
-# # 2000 random points
-# 
-
-# In[7]:
-
-
-# api_meteor_data.to_csv('api_meteor_data.csv')  
-# randomly selects 2000 data points and puts them in data frame
-api_meteor_data = meteor.loc[np.random.randint(len(meteor), size = 2000)]
-# print(api_meteor_data)
-# print(type(api_meteor_data))
-
-
-# # # Select 2000 points and save as CSV 
-
-# # In[14]:
-
-
-# meteor_test = pd.read_csv('meteorite-landings.csv')
-# meteor_test_2000 = meteor.loc[np.random.randint(len(meteor), size = 2000)]
-
-# reader = csv.DictReader(
-#     open("meteor_test_2000.csv"),
-#     fieldnames=['order', 'name', 'id', 'nametype', 'recclass', 'mass', 'fall', 'year', 'reclat', 'reclong', 'GeoLocation']
-# )
-# all_meteors = []
-# for row in reader:
-# #     print(row)
-#     all.append(row)
-# #     print(all, '\n')
-# print(all)
-
-# # all.to_csv('meteor_test_50.csv')
-
-
-# # # Convert to Key : Value pairs
-
-# # In[41]:
-
-
-file = "data/meteor_test_2000.csv"
-
-meteor_kv = []
-
-with open(file, mode='r') as input_file:
-    rows = []
-    for row in input_file:
-        rows.append(row.rstrip('\n').split(","))
-    keys = rows[0]
-    for values in rows[1:]:
-        meteor_kv.append(dict(zip(keys, values)))
-
-pd.set_option("display.max_rows", 1000000)
-pd.set_option("display.max_columns", 1000000)
-# meteor_kv = pd.DataFrame(meteor_kv)
-# print(meteor_kv)
-
-
-meteor_kv ='\n'.join(map(str, meteor_kv))
-
-# # file = "meteor_test_2000.csv"
-# # meteor_kv = []
-# # with open(file, mode='r') as input_file:
-# #     rows = []
-# #     for row in input_file:
-# #         rows.append(row.rstrip('/n').split(","))
-# #     keys = rows[0]
-# #     for values in rows[1:]:
-# #         meteor_kv.append(dict(zip(keys, values)))
-# # print(meteor_kv)
-# # # print(meteor_test_2000)
-
-
-# # # Convert to txt file
-# # 
-
-# # In[46]:
-
-
-# with open('2000_meteors.txt', 'w') as f:
-#     for item in my_list:
-#         f.write("%s\n" % item)
-
-
-# # # Geolocation conversion
-
-# # In[47]:
-
-
-# # Testing Geolocation conversion
-# geo_test = api_meteor_data_arr[2]
-# geolocator = Nominatim(user_agent="api_meteor_data_arr")
-# print(geo_test[9])
-
-# location = geolocator.reverse("52.509669, 13.376294")
-# print(location)
-# coor = str(geo_test[9])[1:-1]
-# print(coor)
-# location = geolocator.reverse(coor)
-# print(location)
-# print(location.address)
-
-
-# # # Testing graph with 2000 points 
-
-# # In[48]:
-
-
-
-# print("Maximum mass: ", np.max(meteor_test_2000['mass']))
-# print("Minimum mass: ", np.min(meteor_test_2000['mass']))
-# print("Mean mass: ",np.mean(meteor_test_2000['mass']))
-
-# # create a figure and axis 
-# fig, ax = plt.subplots() 
-
-# # scatter the sepal_length against the sepal_width
-# ax.scatter(meteor_test_2000['mass'], meteor_test_2000['year'])
-
-# # set a title and labels
-# ax.set_title('Meteor Dataset (mass vs year)')
-# ax.set_xlabel('mass')
-# ax.set_ylabel('year')
-
-
-# # 
-
-# # In[ ]:
-
-
-
-
+# *Conclusions / Questions*
+#     - ...
